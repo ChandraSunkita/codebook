803c116f2aabb9dc74ee92b6841749c1d5a3dbd0
ChandraSunkita <chandrasunkita@gmail.com>
2024-12-01 20:55:47 -0600

    Home page, products page and product details page changes

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,19 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import { Search } from "../Sections/Search";
+
 import Logo from '../../assets/logo.png';
+
 export const Header = () => {
+
+    const [searchSection, setSearchSection] = useState(false);
+    const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
+    useEffect(() => {
+        localStorage.setItem("darkMode",JSON.stringify(darkMode));
+        if(darkMode){
+            document.documentElement.classList.add('dark');
+        }else{
+            document.documentElement.classList.remove('dark');
+        } 
+    }, [darkMode]);
+
   return (
     <header>      
         <nav className="bg-white dark:bg-gray-900">
             <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
-                <Link href="/" className="flex items-center">
+                <Link to="/" className="flex items-center">
                     <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeBook</span>
                 </Link>
                 <div className="flex items-center relative">
-                    <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
-                    <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
-                    <Link href="/cart" className="text-gray-700 dark:text-white mr-5">
+                    <span onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
+                    <span onClick={() => setSearchSection(!searchSection)} className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
+                    <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
                     <span className="text-2xl bi bi-cart-fill relative">
                         <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">0</span>
                     </span>                    
@@ -22,6 +38,7 @@ export const Header = () => {
                 </div>
             </div>
         </nav>
+        {searchSection && <Search setSearchSection={setSearchSection}/> }
     </header>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { Footer } from './Layouts/Footer';
 export { ProductCard } from './Elements/ProductCard';
 export { ScrollToTop } from './Other/ScrollToTop';
 export { Search } from './Sections/Search';
+
+export { Rating } from './Elements/Rating';
