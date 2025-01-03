c33a58d7b579899972739c3d2c11dd2e0a895be9
ChandraSunkita <chandrasunkita@gmail.com>
2025-01-03 12:58:46 -0600

    Dashboard page setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ export { ProductsList } from './Products/ProductsList';
 export { ProductDetail } from './ProductDetail';
 
 export { CartPage } from './Cart/CartPage';
+export { OrderPage } from './Order/OrderPage';
+export { DashboardPage } from './Dashboard/DashboardPage';
 
 export { Login } from './Login';
 export { Register } from './Register';
-
-export { OrderPage } from './Order/OrderPage';
